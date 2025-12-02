57d35c9526024875dd9cea11cfdc26d5528e44e9
gjschoneveld <gertjan935@gmail.com>
2025-12-02 18:42:03 +0100

    Day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,54 @@
+﻿var input = File.ReadAllText("input.txt");
+var ranges = input.Split(',').Select(Parse).ToList();
+
+var answer1 = ranges.SelectMany(r => FindInvalid(r, IsInvalid1)).Sum();
+Console.WriteLine($"Answer 1: {answer1}");
+
+var answer2 = ranges.SelectMany(r => FindInvalid(r, IsInvalid2)).Sum();
+Console.WriteLine($"Answer 2: {answer2}");
+
+bool IsInvalid1(long value)
+{
+    var str = value.ToString();
+    var half = str.Length / 2;
+
+    return str[..half] == str[half..];
+}
+
+bool IsInvalid2(long value)
+{
+    var str = value.ToString();
+    var half = str.Length / 2;
+
+    for (var size = 1; size <= half; size++)
+    {
+        if (str.Chunk(size).Select(chunk => new string(chunk)).Distinct().Count() == 1)
+        {
+            return true; 
+        }
+    }
+
+    return false;
+}
+
+List<long> FindInvalid((long From, long To) range, Func<long, bool> isInvalid)
+{
+    var result = new List<long>();
+
+    for (long id = range.From; id <= range.To; id++)
+    {
+        if (isInvalid(id))
+        {
+            result.Add(id);
+        }
+    }
+    
+    return result;
+}
+
+(long From, long To) Parse(string range)
+{
+    var parts = range.Split('-');
+
+    return (long.Parse(parts[0]), long.Parse(parts[1]));
+}
