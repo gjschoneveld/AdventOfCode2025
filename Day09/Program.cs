506208e51487747d23608a196d5cca416c283f23
Gert Jan Schoneveld <gertjan@facilityapps.com>
2025-12-09 12:25:56 +0100

    Day 9

---
+++ Day09/Program.cs
@@ -0,0 +1,28 @@
+﻿using Point = (long X, long Y);
+
+var input = File.ReadAllLines("input.txt");
+var redTiles = input.Select(Parse).ToList();
+
+var maxSize = 0L;
+
+for (int i = 0; i < redTiles.Count; i++)
+{
+    for (int j = i + 1; j < redTiles.Count; j++)
+    {
+        maxSize = Math.Max(maxSize, Size(redTiles[i], redTiles[j]));
+    }
+}
+
+Console.WriteLine($"Answer 1: {maxSize}");
+
+long Size(Point a, Point b)
+{
+    return (Math.Abs(a.X - b.X) + 1) * (Math.Abs(a.Y - b.Y) + 1);
+}
+
+Point Parse(string line)
+{
+    var values = line.Split(',').Select(long.Parse).ToList();
+
+    return (values[0], values[1]);
+}
